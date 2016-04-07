c5b105b88f0dcf6ff782e5eed26d0675e5453847
Nigel J. Hindley <nigel.hindley@outlook.com>
2016-04-07 13:27:36 +0100

    Base code

---
+++ Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace baseProject01
+{
+    public partial class frmForm01 : Form
+    {
+        public frmForm01()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+
+        {
+            this.Close();
+        }
+    }
+}
